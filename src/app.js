eed7f9ddef9b18e44e665c96ac06f1e83c749565
LostLord07 <satishsingh45264209@gmail.com>
2025-01-16 20:35:27 +0530

    initital backend setup

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import logger from "./logger.js";
+import morgan from "morgan";
+const morganFormat = ":method :url :status :response-time ms";
+
+const app = express();
+
+
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+);
+
+// common middlewares
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, 
+}))
+
+app.use(express.json({
+    limit: "16kb"
+})); 
+
+app.use(express.urlencoded({
+    extended: true, 
+    limit: "16kb"
+})); 
+
+app.use(express.static("public"));
+
+
+export {app}; 
